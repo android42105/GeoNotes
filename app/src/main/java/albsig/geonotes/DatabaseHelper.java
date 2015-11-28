e76692bf8d82732215626422dfec15f6d6779683
burghajo <burghajo@hs-albsig.de>
2015-11-28 13:07:00 +0100

    add database.

---
+++ app/src/main/java/albsig/geonotes/DatabaseHelper.java
@@ -0,0 +1,47 @@
+package albsig.geonotes;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import static albsig.geonotes.DatabaseContract.*;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    // If you change the database schema, you must increment the database version.
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "GeoNotesDATABASE.db";
+
+
+    //SQL statements
+    private final String SQL_CREATE_ENTRIES = "CREATE TABLE " +
+            FeedEntry.TABLE_NAME + " (" +
+            FeedEntry._ID + " INTEGER PRIMARY KEY," +
+            FeedEntry.COLUMN_NAME_TITLE + " TEXT," +
+            FeedEntry.COLUMN_NAME_NOTE + " TEXT," +
+            FeedEntry.COLUMN_NAME_LOCATION + " TEXT" +
+            " );";
+
+
+    private final String SQL_DELETE_TABLE = "DROP TABLE IF EXISTS " + FeedEntry.TABLE_NAME;
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.d("IN DATABASEHELPER", " onCREATE HAS BEEN CALLED");
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQL_DELETE_TABLE);
+        onCreate(db);
+    }
+}
--- app/src/main/java/albsig/geonotes/dbActivity.java
+++ app/src/main/java/albsig/geonotes/dbActivity.java
@@ -1,12 +1,20 @@
 package albsig.geonotes;
 
-import android.app.ActionBar;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import static albsig.geonotes.DatabaseContract.*;
 
 public class dbActivity extends AppCompatActivity {
 
@@ -15,10 +23,19 @@ public class dbActivity extends AppCompatActivity {
     private DisplayMetrics metrics;
     private int MIN_DISTANCE;
 
+    private DatabaseHelper dbhelper;
+    private SQLiteDatabase dbase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_db);
+
+        this.dbhelper = new DatabaseHelper(this);
+        this.dbase = dbhelper.getReadableDatabase();
+
+        readFromDatabase();
+
     }
 
     @Override
@@ -72,6 +89,34 @@ public class dbActivity extends AppCompatActivity {
 
     private void swipeLeft2Right() {
         onBackPressed();
+    }
+
+    private void readFromDatabase() {
+
+        ScrollView sv = (ScrollView) findViewById(R.id.scrollView);
+        LinearLayout ll = new LinearLayout(this);
+        ll.setOrientation(LinearLayout.VERTICAL);
+
+        String[] projections = {FeedEntry._ID, FeedEntry.COLUMN_NAME_TITLE, FeedEntry.COLUMN_NAME_NOTE,
+                FeedEntry.COLUMN_NAME_LOCATION
+        };
+
+        Cursor c = dbase.query(
+                FeedEntry.TABLE_NAME, projections, null, null, null, null, null);
+
+        boolean goon = true;
+        while (goon) {
+            goon = c.moveToNext();
+            TextView bra = new TextView(this);
+            bra.setText(c.getPosition() + "das ist aus der datenbank");
+            ll.addView(bra);
+        }
+        sv.addView(ll);
+        c.moveToLast();
+        Log.d("DB ACTIVITY", c.getPosition() + " ");
+        int n = c.getPosition();
+
+
     }
 
 }
