ebd4beabe4564c39bd44c03a8fdea8cf15b39214
Fabian <rudzinfa@hs-albsig.de>
2015-12-03 18:10:32 +0100

    Margin between TextViews in DBActivity

--- app/src/main/java/albsig/geonotes/DBActivity.java
+++ app/src/main/java/albsig/geonotes/DBActivity.java
@@ -1,18 +1,27 @@
 package albsig.geonotes;
 
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Typeface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.StyleSpan;
 import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridLayout;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import static albsig.geonotes.DatabaseContract.*;
 
@@ -87,24 +96,77 @@ public class DBActivity extends AppCompatActivity {
 
         Cursor c = dbase.query(
                 FeedEntry.TABLE_NAME, projections, null, null, null, null, null);
-        c.moveToFirst();
         boolean goon = true;
         while (c.moveToNext()) {
-            final String[] strA = new String[2];
             final TextView bra = new TextView(this);
-            int id = c.getInt(0);
-            strA[0] = c.getString(1);
-            strA[1] = c.getString(2);
-            bra.setText("ID: " + id + "\n Title: " + strA[0] + "  Node: " + strA[1]);
+
+            final int id = c.getInt(0);
+            final String title = c.getString(1);
+            final String note = c.getString(2);
+
+            bra.setText("Title\n" + title + "\n\nNote\n" + note);
             bra.setBackgroundResource(R.drawable.db_textview_shape);
 
             bra.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(DBActivity.this);
+                    dialogBuilder.setView(R.layout.dialog_edit);
+                    final AlertDialog dialog = dialogBuilder.create();
+                    dialog.setCanceledOnTouchOutside(false);
+                    dialog.show();
 
+
+                    final Button dialogSaveButton = (Button) dialog.findViewById(R.id.dialogEditSave);
+                    final Button dialogDeleteButton = (Button) dialog.findViewById(R.id.dialogEditDelete);
+                    final Button dialogCancelButton = (Button) dialog.findViewById(R.id.dialogEditCancel);
+                    final Button dialogShowButton = (Button) dialog.findViewById(R.id.dialogEditShow);
+
+
+                    final EditText dialogTitle = (EditText) dialog.findViewById(R.id.dialogEditTitle);
+                    final EditText dialogNote = (EditText) dialog.findViewById(R.id.dialogEditNote);
+
+                    dialogTitle.setHint(title);
+                    dialogNote.setHint(note);
+
+                    dialogCancelButton.setOnClickListener(new View.OnClickListener() {
+                        public void onClick(View v) {
+                            dialog.cancel();
+                        }
+                    });
+
+                    dialogSaveButton.setOnClickListener(new View.OnClickListener() {
+                        public void onClick(View v) {
+                            dialog.cancel();
+                            DBActivity.this.dbhelper = new DatabaseHelper(DBActivity.this);
+                            DBActivity.this.dbase = dbhelper.getWritableDatabase();
+
+                            ContentValues values = new ContentValues();
+                            values.put(FeedEntry.COLUMN_NAME_TITLE, title);
+                            values.put(FeedEntry.COLUMN_NAME_NOTE, note);
+                            values.put(FeedEntry.COLUMN_NAME_LOCATION, "location test");
+
+                            //TODO Update in Database
+                        }
+                    });
+
+                    dialogDeleteButton.setOnClickListener(new View.OnClickListener() {
+                        public void onClick(View v) {
+                            //TODO Delete Button functionality
+                        }
+                    });
+
+                    dialogShowButton.setOnClickListener(new View.OnClickListener() {
+                        public void onClick(View v) {
+                            //TODO Show on Map Button functionality
+                        }
+                    });
                 }
             });
 
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(new AbsListView.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            params.setMargins(10, 10, 10, 10);
+            bra.setLayoutParams(params);
             ll.addView(bra);
         }
         this.sv.addView(ll);
